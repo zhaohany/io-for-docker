9c151b56e9c5f5417d48c97fbbae69aa5831deed
zhaohany <zhaohany@outlook.com>
2023-08-02 14:54:12 -0400

    feat():test and result update

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 echo
 echo Testing Sequential Read ...
 FIO_RW=read
-READ_IOPS=$(fio --name=read_iops --fallocate=none --runtime=5 --directory=$MOUNTPOINT --filename_format=$jobnum.dat  --blocksize=4096 --time_based=1 --ioengine=libaio --iodepth=8 --direct=1 --buffered=0 --thread=0 --numjobs=4 --group_reporting=1 --filesize=2g --size=2g --rw=${FIO_RW})
+READ_IOPS=$(fio --name=read_iops --fallocate=none --runtime=30 --directory=$MOUNTPOINT --filename_format=$jobnum.dat  --blocksize=4096 --time_based=1 --ioengine=libaio --iodepth=8 --direct=1 --buffered=0 --thread=0 --numjobs=4 --group_reporting=1 --filesize=8g --size=8g --rw=${FIO_RW})
 echo ==========================
 echo = Sequential Read Result =
 echo ==========================
@@ -13,7 +13,7 @@ echo
 echo
 echo Testing Random Read ...
 FIO_RW=randread
-RANDREAD_IOPS=$(fio --name=randread_iops --fallocate=none --runtime=5 --directory=$MOUNTPOINT --filename_format=$jobnum.dat  --blocksize=4096 --time_based=1 --ioengine=libaio --iodepth=8 --direct=1 --buffered=0 --thread=0 --numjobs=4 --group_reporting=1 --filesize=2g --size=2g --rw=${FIO_RW})
+RANDREAD_IOPS=$(fio --name=randread_iops --fallocate=none --runtime=30 --directory=$MOUNTPOINT --filename_format=$jobnum.dat  --blocksize=4096 --time_based=1 --ioengine=libaio --iodepth=8 --direct=1 --buffered=0 --thread=0 --numjobs=4 --group_reporting=1 --filesize=8g --size=8g --rw=${FIO_RW})
 echo ======================
 echo = Random Read Result =
 echo ======================
@@ -23,7 +23,7 @@ echo
 echo
 echo Testing Sequential Write ...
 FIO_RW=write
-WRITE_IOPS=$(fio --name=write_iops --fallocate=none --runtime=5 --directory=$MOUNTPOINT --filename_format=$jobnum.dat  --blocksize=4096 --time_based=1 --ioengine=libaio --iodepth=8 --direct=1 --buffered=0 --thread=0 --numjobs=4 --group_reporting=1 --filesize=2g --size=2g --rw=${FIO_RW})
+WRITE_IOPS=$(fio --name=write_iops --fallocate=none --runtime=30 --directory=$MOUNTPOINT --filename_format=$jobnum.dat  --blocksize=4096 --time_based=1 --ioengine=libaio --iodepth=8 --direct=1 --buffered=0 --thread=0 --numjobs=4 --group_reporting=1 --filesize=8g --size=8g --rw=${FIO_RW})
 echo ===========================
 echo = Sequential Write Result =
 echo ===========================
@@ -33,7 +33,7 @@ echo
 echo
 echo Testing Random Write ...
 FIO_RW=randwrite
-RANDWRITE_IOPS=$(fio --name=randwrite_iops --fallocate=none --runtime=5 --directory=$MOUNTPOINT --filename_format=$jobnum.dat  --blocksize=4096 --time_based=1 --ioengine=libaio --iodepth=8 --direct=1 --buffered=0 --thread=0 --numjobs=4 --group_reporting=1 --filesize=2g --size=2g --rw=${FIO_RW})
+RANDWRITE_IOPS=$(fio --name=randwrite_iops --fallocate=none --runtime=30 --directory=$MOUNTPOINT --filename_format=$jobnum.dat  --blocksize=4096 --time_based=1 --ioengine=libaio --iodepth=8 --direct=1 --buffered=0 --thread=0 --numjobs=4 --group_reporting=1 --filesize=8g --size=8g --rw=${FIO_RW})
 echo =======================
 echo = Random Write Result =
 echo =======================
